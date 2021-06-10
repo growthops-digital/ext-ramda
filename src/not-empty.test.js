654b92106dff7976ad1528096fde0a407aee36e3
Tristan Strathearn <tristan.strathearn@growthops.com.au>
2021-06-10 17:11:56 +1000

    feat: add notEmpty function

---
+++ src/not-empty.test.js
@@ -0,0 +1,29 @@
+import notEmpty from './not-empty.js';
+
+test('array with items is not empty', () => {
+	const input = [1, 2, 3];
+	const result = notEmpty(input);
+
+	expect(result).toBe(true);
+});
+
+test('default array returns as empty', () => {
+	const input = [];
+	const result = notEmpty(input);
+
+	expect(result).toBe(false);
+});
+
+test('string containing characters is not empty', () => {
+	const input = 'Hello world';
+	const result = notEmpty(input);
+
+	expect(result).toBe(true);
+});
+
+test('blank string returns as empty', () => {
+	const input = 'Hello world';
+	const result = notEmpty(input);
+
+	expect(result).toBe(true);
+});
