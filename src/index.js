d512a5abf4409550fa9b7df16d43eb09b1d3cc56
Tristan Strathearn <56568247+tristan-go@users.noreply.github.com>
2021-06-15 09:07:51 +1000

    feat: add ternary function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as notEmpty} from './not-empty.js';
 export {default as notEquals} from './not-equals.js';
 export {default as reform} from './reform.js';
+export {default as ternary} from './ternary.js';
